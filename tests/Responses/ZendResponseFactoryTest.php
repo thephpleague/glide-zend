29cc75df4170a2ff902cd9f2bfdd675db0a91684
Rudolph Gottesheim <r.gottesheim@loot.at>
2018-10-17 20:37:59 +0200

    Update a test to reflect a change in league/glide

--- tests/Responses/ZendResponseFactoryTest.php
+++ tests/Responses/ZendResponseFactoryTest.php
@@ -27,7 +27,10 @@ class ZendResponseFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Zend\Diactoros\Response', $response);
         $this->assertEquals('image/jpeg', $response->getHeaderLine('Content-Type'));
         $this->assertEquals('5175', $response->getHeaderLine('Content-Length'));
-        $this->assertContains(gmdate('D, d M Y H:i', strtotime('+1 years')), $response->getHeaderLine('Expires'));
+        $this->assertContains(
+            date_create('+1 years')->format('D, d M Y H:i:s') . ' GMT',
+            $response->getHeaderLine('Expires')
+        );
         $this->assertEquals('max-age=31536000, public', $response->getHeaderLine('Cache-Control'));
     }
 }
